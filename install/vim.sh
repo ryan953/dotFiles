5a204cac25146f529878f2528e9da655c52b3fe0
Ryan Albrecht <ryan@ryanalbrecht.ca>
2019-08-11 17:42:37 -0700

    install onedark vim theme

--- install/vim.sh
+++ install/vim.sh
@@ -5,13 +5,14 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 VIM="$HOME/.vim/"
 
 mkdir -p "$VIM"
-mkdir -p "$VIM/tmp"
+mkdir -p "${VIM}tmp"
+mkdir -p "${VIM}colors"
 
 link_if() {
 	local FILE=$1
 	local DEST=$2
 
-	BASE=`basename "$FILE"`
+	BASE=$(basename "$FILE")
 
 	if [ -h "$DEST$BASE" ]; then
 		rm "$DEST$BASE"
@@ -20,12 +21,24 @@ link_if() {
 	echo "Linked $FILE to $DEST$BASE"
 }
 
-link_if `pwd`/submodules/vim-one/colors "$VIM"
-link_if `pwd`/submodules/vim-pathogen/autoload "$VIM"
+# Link pathogen/autoload so it can find everything in "${VIM}/bundle"
+link_if "$(pwd)/submodules/vim-pathogen/autoload" "$VIM"
 
-DEST=""$VIM"bundle/"
+DEST="${VIM}bundle/"
 mkdir -p "$DEST"
 for FILE in `pwd`/submodules/vim-bundle/*; do
-	BASE=`basename "$FILE"`
+	# Link everything from vim-bundle so pathogen finds it
+	BASE=$(basename "$FILE")
 	link_if "$FILE" "$DEST"
+
+	# Also link any color theme and put them in ${VIM}/colors
+	if [[ -d "$FILE/colors" ]] ; then
+		for COLORFILE in $FILE/colors/*; do
+			link_if "$COLORFILE" "${VIM}colors/"
+		done
+	fi
 done
+
+# This submodule was really installed to the wrong place, so link it manually
+# Next time move it to vim-bundles
+link_if $(pwd)/submodules/vim-one/colors/one.vim "${VIM}colors/"
