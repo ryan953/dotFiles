658a3cfd8de08c405c17342928e95be8ef465edf
Ryan Albrecht <ryan@ryanalbrecht.ca>
2021-03-14 15:18:12 -0700

    Add a message to display arch type when installing osx packages

--- install-zsh.sh
+++ install-zsh.sh
@@ -88,6 +88,19 @@ init () {
   OS="$(uname)"
   case $OS in
     Darwin)
+      arch_name="$(uname -m)"
+      if [ "${arch_name}" = "x86_64" ]; then
+        if [ "$(sysctl -in sysctl.proc_translated)" = "1" ]; then
+          echo "Running on Rosetta 2"
+        else
+          echo "Running on native Intel"
+        fi
+      elif [ "${arch_name}" = "arm64" ]; then
+        echo "Running on ARM"
+      else
+        echo "Unknown architecture: ${arch_name}"
+      fi
+
       echo "##### Install Apple Command Line Tools"
       xcode-select --install || true
 
